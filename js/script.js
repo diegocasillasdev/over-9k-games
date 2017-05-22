941a8ae91b66890a080a7a19268c4b80936c906f
Diego <diego@localhost.localdomain>
2017-05-23 00:37:54 +0200

    Añade fichas

--- js/script.js
+++ js/script.js
@@ -67,8 +67,52 @@ var fichas = [
         miniatura3: "img/doom-miniatura3.jpg",
         miniatura4: "img/doom-miniatura4.jpg",
     },
-    ["tiny", "img/tiny.png", "img/tinygif.gif"],
-    ["rocket", "img/rocket.jpg", "img/rocketgif.gif"]
+
+    {
+        id: "tiny", // #id
+        titulo: "Tiny and Big", // Título
+
+        /* PLATAFORMAS */
+
+        portada: "img/tinycaratula.jpg", // Portada
+        pegi: "img/7.gif", // PEGI
+        desarrollador: "Black Pants Studio", // Desarrollador
+        genero: "Indie", // Género
+        precio: "9,99€", // Precio
+        fecha: "19-6-2012", // Fecha
+
+        /* DESCRIPCION */
+        descripcion: "¡Tiny & Big, un plataformas de saltar y cortar de estilo comic, te da la única habilidad de dar forma a un mundo entero como desees! Eres Tiny, un inventor nerd que trata de recuperar su más preciada posesión: ¡los calzoncillos blancos de fina costura del abuelo! En su viaje a través de un desierto olvidado conocerá misteriosas criaturas, no payasos, un robot taxi y su archienemigo: ¡Big!",
+
+        /* MINIATURAS */
+        miniatura1: "img/tiny-miniatura1.jpg",
+        miniatura2: "img/tiny-miniatura2.jpg",
+        miniatura3: "img/tiny-miniatura3.jpg",
+        miniatura4: "img/tiny-miniatura4.jpg",
+    },
+
+    {
+        id: "rocket", // #id
+        titulo: "Rocket League", // Título
+
+        /* PLATAFORMAS */
+
+        portada: "img/rocketcaratula.jpg", // Portada
+        pegi: "img/3.gif", // PEGI
+        desarrollador: "Psyonix", // Desarrollador
+        genero: "Carreras, Deportes, Arcade", // Género
+        precio: "19,99€", // Precio
+        fecha: "7-7-2015", // Fecha
+
+        /* DESCRIPCION */
+        descripcion: "¡El fútbol conoce a la conducción una vez más en la largamente esperada, basada en físicas y enfocada al multijugador secuela de Supersonic Acrobatic Rocket-Powered Battle-Cars! ¡Elige una variedad de vehículos voladores equipados con enormes rocket boosters para marcar goles aéreos alucinantes y sacarte de la manga increíbles paradas para cambiar el partido!",
+
+        /* MINIATURAS */
+        miniatura1: "img/rocket-miniatura1.jpg",
+        miniatura2: "img/rocket-miniatura2.jpg",
+        miniatura3: "img/rocket-miniatura3.jpg",
+        miniatura4: "img/rocket-miniatura4.jpg",
+    }
 ]
 
 /* Indica si la imagen del slider ha sido clicada */
@@ -141,11 +185,6 @@ $(".imagen-juego").click(function () {
             break;
         }
     }
-
-
-
-
-
 });
 
 
