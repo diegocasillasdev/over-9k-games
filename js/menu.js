2990814ec63b66d3d8fbe53ccdc5a1b2998d4d9a
Diego <diego@localhost.localdomain>
2017-05-24 17:03:06 +0200

    Arregla menú

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,11 @@
 /* Eventos para mostrar y ocultar el submenu en hover y click */
 
 $(".menu-principal > li").hover(function () {
+    $(this).children(".submenu").stop();
     $(this).children(".submenu").slideDown("fast");
+    
 }, function () {
+    $(this).children(".submenu").stop();
     $(this).children(".submenu").slideUp("fast");
 });
 
