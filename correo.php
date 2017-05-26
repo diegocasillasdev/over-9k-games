cbeb6d9853f6752690c30fd423c66dfc889aaa73
Diego <diego@localhost.localdomain>
2017-05-26 21:00:33 +0200

    Prueba correo

--- correo.php
+++ correo.php
@@ -7,15 +7,15 @@
  */
 
 if($_POST){
-   echo "Los datos recibidos son: <br>Nombre: ".$_POST['name']."<br> "
-            . "email: ".$_POST['email']."<br> Mensaje: ".$_POST['message'];
-			
+   echo "Los datos recibidos son: <br>Nombre: ".$_POST['nombre']."<br> "
+            . "email: ".$_POST['email']."<br> Mensaje: ".$_POST['mensaje'];
+	/*		
   $destino= "diegocasillasbragado@gmail.com";
   $remitente = $_POST['email'];
   $asunto = "Mensaje enviado por: ".$_POST['nombre'];
   $mensaje= $_POST['mensaje'];
   mail($destino,$asunto,$mensaje,"FROM: $remitente");
-  header("location:formulariocorrecto.html");
+  header("location:formulariocorrecto.html");*/
 }
 else{
     echo "No se ha podido enviar el correo.";
